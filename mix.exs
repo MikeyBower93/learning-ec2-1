71f079918ab118f11b257bddf5a3d38edbb7a804
MikeyBower93 <mikey.bower93@gmail.com>
2020-11-15 22:03:01 +0000

    Set elixir version.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LearningEc21.MixProject do
     [
       app: :learning_ec2_1,
       version: "0.1.0",
-      elixir: "~> 1.9.4",
+      elixir: "~> 1.10.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
